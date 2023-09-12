33ee13adc6162d6ab186ab4cbef70452a94aea7e
Semetery29 <mdazizulhakimsuny@gmail.com>
2023-09-13 01:32:33 +0600

    data is now showing

--- src/Components/Todos.tsx
+++ src/Components/Todos.tsx
@@ -1,7 +1,22 @@
 "use client";
 
+import { useTodos } from "@/store/todos";
+
 const Todos = () => {
-  return <div></div>;
+  const { todos } = useTodos();
+  console.log(todos);
+
+  let filterTodos = todos;
+
+  return <ul>
+    {
+        filterTodos.map(todo => {
+            return <li key={todo.id}>
+                {todo.task}
+            </li>
+        })
+    }
+  </ul>;
 };
 
 export default Todos;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import AddToDo from "@/Components/AddToDo";
+import Todos from "@/Components/Todos";
 
 const page = () => {
   return (
     <main>
       <h2>NEXT + TypeScript </h2>
       <AddToDo />
+      <Todos />
     </main>
   );
 };
